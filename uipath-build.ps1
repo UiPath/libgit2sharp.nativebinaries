f2954dcde3ff13b931fa4a3f38aafac88dac0d04
Andrei Balint <andrei.balint@uipath.com>
2019-06-10 10:04:30 +0300

    Build libssh2 with openssl support

--- uipath-build.ps1
+++ uipath-build.ps1
@@ -1,3 +1,20 @@
-$version =  gc .\libgit2\include\git2\version.h | %{ [regex]::matches($_, '(\d+.\d+.\d+)') } | %{ $_.Groups[1].Value }
+function Test-Command($cmdname)
+{
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+if (!(Test-Command -cmdname 'gitversion'))
+{
+	Import-Module ./Get-Version.ps1
+	Get-Version 
+}
+
+$verinfo = gitversion  /output json /nofetch | out-string
+$jsonver = try { ConvertFrom-Json $verinfo } catch {}
+$version = $jsonver.NuGetVersion	
+
+$version
+
 .\build.libgit2.ps1
 .\buildpackage.ps1 "$version-ssh"
+
